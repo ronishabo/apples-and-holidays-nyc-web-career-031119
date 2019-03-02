ed93f72bac5d5291c66430e3664387b9a8b2c59b
Roni Shabo <roni.shabo@gmail.com>
2019-03-02 21:02:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,7 +16,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash[:summer]
+  holiday_hash[:spring][:memorial_day]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
