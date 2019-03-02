d4a5fd41b5b8ce71d18c9d9ebbc237c61768d72f
Roni Shabo <roni.shabo@gmail.com>
2019-03-02 21:22:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash.include?(supply)
+  holiday_hash.include?
 end
 
 def all_supplies_in_holidays(holiday_hash)
