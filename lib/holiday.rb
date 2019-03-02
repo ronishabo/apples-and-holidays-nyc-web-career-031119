55c7d4d9c31cfeee7c791c423059a69169d21fb5
Roni Shabo <roni.shabo@gmail.com>
2019-03-02 22:06:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.map do |x|
+    x
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
