f9421398c03ecea112801af52bd52c7c3aa59cb1
Roni Shabo <roni.shabo@gmail.com>
2019-03-02 22:13:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.map do |seasons, data|
     data.map do |holiday, supply|
-      array = holiday
+      array = holiday.join.split
     end
   end
 end
